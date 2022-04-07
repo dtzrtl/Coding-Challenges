f57f8de2feece7754928d7c82ba9f00e89fbf4a3
dtzrtl <dtzrtl@gmail.com>
2022-04-06 18:39:24 -0700

    completed challenge 4/6/2022

--- 7 kyu/List Filtering/app.js
+++ 7 kyu/List Filtering/app.js
@@ -5,7 +5,22 @@ filter_list([1,2,'a','b']) == [1,2]
 filter_list([1,'a','b',0,15]) == [1,0,15]
 filter_list([1,2,'aasf','1','123',123]) == [1,2,123] */
 
-function filter_list(l) {
+test = [1,2,'a','b']
+
+ans = [1,2]
+
+/* function filter_list(array) {
     // Return a new array with the strings filtered out
-    
+    filtered = []
+    for(let i = 0; i<array.length; i++){
+        if( typeof array[i] !== 'string')
+        filtered.push(array[i])
+    }
+    return filtered
+  } */
+
+  function filter_list(array) {
+    return array.filter( element => typeof element == 'number')
   }
+
+console.log(filter_list(test),ans)
---
+++ 7 kyu/Simple Fun 176 Reverse Letter/app.js
@@ -0,0 +1,24 @@
+/* Task
+Given a string str, reverse it omitting all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string */
+
+input = "ultr53o?n"
+
+output = "nortlu"
+
+
+function reverseLetter(str) {
+    return str.split("").reverse().filter( c => c.toLowerCase() != c.toUpperCase() ).join("")
+  }
+
+console.log(reverseLetter(input),output)
