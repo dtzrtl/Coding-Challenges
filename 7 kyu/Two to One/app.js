48e158765554f8404372b73d9f4b21af8ace0e75
dtzrtl <dtzrtl@gmail.com>
2022-04-28 22:10:17 -0700

    started challenge 4/28/2022

---
+++ 7 kyu/Two to One/app.js
@@ -0,0 +1,25 @@
+/* 
+Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+*/
+
+let input = ["xyaabbbccccdefww", "xxxxyyyyabklmopq"];
+let output = "abcdefklmopqwxy";
+
+function longest(s1, s2) {
+  return 0;
+}
+
+console.log(longest(input), output);
+console.log(longest(input) === output);
+
+/* 
+Other Solutions
+*/
