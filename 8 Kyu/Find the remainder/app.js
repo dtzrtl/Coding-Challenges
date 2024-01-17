62b26835fa28d0448dba7d5c9571a630a3bf3170
dtzrtl <dtzrtl@gmail.com>
2024-01-16 16:07:48 -0800

    Create app.js

---
+++ 8 Kyu/Find the remainder/app.js
@@ -0,0 +1,21 @@
+/* 
+
+Description
+
+*/
+
+let input = 0;
+let output = 0;
+
+function solution(a, b) {
+  return a > b ? a % b : b % a;
+}
+
+console.log(solution(input), output);
+console.log(solution(input) === output);
+
+/* 
+
+Other Solutions
+
+*/
