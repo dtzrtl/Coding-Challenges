21bd236abf327098a761bda2d82ac35569ce0ba6
dtzrtl <dtzrtl@gmail.com>
2024-01-17 19:56:37 -0800

    completed challenge

---
+++ 8 Kyu/Removing Elements/app.js
@@ -0,0 +1,19 @@
+/* 
+
+Description
+
+*/
+
+let input = 0;
+let output = 0;
+
+const solution = (arr) => arr.filter((_, i) => i % 2 == 0);
+
+console.log(solution(input), output);
+console.log(solution(input) === output);
+
+/* 
+
+Other Solutions
+
+*/
